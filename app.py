f6c796fa5160331ec43b78c7dedec455db193ba0
Matuwamai <wamaimatu@gmail.com>
2025-10-01 21:50:07 +0300

    Perform logical operation by the operator

---
+++ app.py
@@ -0,0 +1,29 @@
+# # and operator
+# high_income = False
+# good_creadit = True
+
+# if high_income and good_creadit:
+#     print("Eligible")
+
+# else:
+#     print("Not eligilbe")
+#     # or operator
+
+# high_income = False
+# good_creadit = True
+
+# if high_income or good_creadit:
+#     print("Eligible")
+
+# else:
+#     print("Not eligilbe")
+
+# not operator
+high_income = False
+good_creadit = True
+student = False
+
+if (high_income or good_creadit) and not student:
+    print("Eligible")
+else:
+    print("not eliginle")
